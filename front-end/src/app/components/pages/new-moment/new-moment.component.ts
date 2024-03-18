d527d9c67059cc0af34d418fad1b0c46f2ca7eeb
David Oliveira <davidolisoa@gmail.com>
2024-03-18 17:34:00 -0300

    Recriando Front-end

---
+++ front-end/src/app/components/pages/new-moment/new-moment.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { Moment } from 'src/app/Moment';
+import { MomentService } from 'src/app/service/moment.service';
+
+@Component({
+  selector: 'app-new-moment',
+  templateUrl: './new-moment.component.html',
+  styleUrls: ['./new-moment.component.css']
+})
+export class NewMomentComponent {
+
+  btnText = "Compartilhar";
+
+  constructor(private momentService: MomentService) {}
+
+  async createHandler(moment: Moment) {
+    const formData = new FormData();
+
+    formData.append('title', moment.title);
+    formData.append('description', moment.description);
+
+    if(moment.image) {
+      formData.append('image', moment.image);
+    }
+
+    await this.momentService.createMoment(formData).subscribe();
+  }
+
+}
