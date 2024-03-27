519f5c9aa1f5c689aecb73eacde0aea147412fe7
David Oliveira <davidolisoa@gmail.com>
2024-03-27 16:21:48 -0300

    Começando de novo

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,33 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { AboutComponent } from './components/pages/about/about.component';
-import { HomeComponent } from './components/pages/home/home.component';
-import { NewMomentComponent } from './components/pages/new-moment/new-moment.component';
-import { MomentFormComponent } from './components/moment-form/moment-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    AboutComponent,
-    HomeComponent,
-    NewMomentComponent,
-    MomentFormComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    FormsModule,
-    ReactiveFormsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
