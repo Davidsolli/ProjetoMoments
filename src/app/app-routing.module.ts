29e69636cbad940802bb5aa8bf780de0b4b2ee99
Davidsolli <davidolisoa@gmail.com>
2024-07-27 17:21:55 -0300

    estrutura básica do sistema e iniciando css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/pages/home/home.component';
 import { AboutComponent } from './components/pages/about/about.component';
+import { NewMomentsComponent } from './components/pages/new-moments/new-moments.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'about', component: AboutComponent}
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'moments/new', component: NewMomentsComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/components/pages/new-moments/new-moments.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-moments',
+  templateUrl: './new-moments.component.html',
+  styleUrl: './new-moments.component.css'
+})
+export class NewMomentsComponent {
+
+}
