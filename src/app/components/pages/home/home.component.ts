c1da49a03ceee85cd490f9064774dff1fe8d109e
Davidsolli <davidolisoa@gmail.com>
2024-08-03 20:50:17 -0300

    buscando dados da api

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { MomentsService } from './../../../services/moments.service';
+import { Component, OnInit } from '@angular/core';
+import { Moments } from '../../../Moments';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  allMoments: Moments[] = [];
+  moments: Moments[] = [];
+  baseApiUrl = 'http://localhost:3333/';
 
+  constructor(private momentsService: MomentsService) {}
+
+  ngOnInit(): void {
+    this.momentsService.getMoments().subscribe((items) => {
+      const data = items.data;
+
+      data.map((item) => {
+        item.created_at = new Date(item.created_at!).toLocaleDateString('pt-BR');
+      });
+      this.allMoments = data;
+      this.moments = data;
+    });
+  }
 }
--- src/app/services/moments.service.ts
+++ src/app/services/moments.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Response } from '../Response';
+import { Moments } from '../Moments';
 
 @Injectable({
   providedIn: 'root',
@@ -10,6 +12,10 @@ export class MomentsService {
   private apiUrl = `${this.baseApiUrl}api/moments`;
 
   constructor(private http: HttpClient) {}
+
+  getMoments(): Observable<Response<Moments[]>> {
+    return this.http.get<Response<Moments[]>>(this.apiUrl);
+  }
 
   createMoment(formData: FormData): Observable<FormData> {
     return this.http.post<FormData>(this.apiUrl, formData);
